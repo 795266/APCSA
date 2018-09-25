a52beb618415a6cc8bdc295cfe4aedb340603e72
Nathan Bowman <nbowman@WS-ACADBUS.EDU>
2018-09-25 10:36:54 -0700

    A little more into Activity 2

--- Activity 2/Activity 2/BallRunner.java
+++ Activity 2/Activity 2/BallRunner.java
@@ -26,8 +26,8 @@ public class BallRunner
         TGPoint entrancepoint = new TGPoint(0,0);
         BallBot[] ballBotArray = new BallBot[10];
         BallRunner ballRunner = new BallRunner();
-
-        findFreeBallBotIndex(ballBotArray[]);
+        
+        ballRunner.findFreeBallBotIndex(ballBotArray);
     }
 
     /**
@@ -45,15 +45,14 @@ public class BallRunner
             }
     }
      */
-    public static void run(Ballbot[] x) {
-
-    }
+    
     public int findFreeBallBotIndex(BallBot[] x) {
-        for(int i = 0; i < x.length(); i = i = 1) {
+        int ret = x.length;
+        for(int i = 0; i < x.length; i = i = 1) {
             if(x[i] == null) {
-                return i;
+                ret = i;
             }
         }
-        return x.length;
+        return ret;
     }
 }
