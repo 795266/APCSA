5e88f1e106993310d619e3e7069885e5d27e0834
795266 <42599270+795266@users.noreply.github.com>
2018-10-01 10:19:18 -0700

    Finish Activity3, no progress on Activity 4

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -7,38 +7,31 @@
  */
 public class BallRunner
 {
-    /**
-     * Constructor for objects of class BallRunner
-     */
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public static void main() {
         BallWorld ballWorld = new BallWorld(200, 200);
         TGPoint entrancePoint = new TGPoint(0,0);
         BallBot[] ballBotArray = new BallBot[10];
         BallRunner ballRunner = new BallRunner();
-        int i = 0;
-        while(i < 1) {
-            int freeBallBotIndex;
-            freeBallBotIndex = ballRunner.findFreeBallBotIndex(ballBotArray);
-            if (freeBallBotIndex < ballBotArray.length) {
-                BallBot ballBot = new BallBot(ballWorld, entrancePoint, (int)(Math.random() * 360), 20);
-                ballBotArray[freeBallBotIndex] = ballBot;
+        boolean i = true;
+        while(i == true) {
+            if(ballRunner.entranceClear(ballBotArray, entrancePoint) == true) {
+                int freeBallBotIndex;
+                freeBallBotIndex = ballRunner.findFreeBallBotIndex(ballBotArray);
+                if (freeBallBotIndex < ballBotArray.length) {
+                    BallBot ballBot = new BallBot(ballWorld, entrancePoint, (int)(Math.random() * 360), 20);
+                    ballBotArray[freeBallBotIndex] = ballBot;
+                }
             }
             for(int index = 0; index < ballBotArray.length; index++) {
                 if (ballBotArray[index] != null) {
                     if(ballBotArray[index].canMoveForward(ballWorld) == true){
                         ballBotArray[index].moveForward();
                     } else {
-                        ballBotArray[index].setHeading((ballBotArray[index].getHeading() + Math.random() * 1000) % 360);
+                        ballBotArray[index].setHeading((Math.random() * 360));
                     }
                 }
             }
+            
         }
     }
     
@@ -62,11 +55,28 @@ public class BallRunner
     }
 
     public int findFreeBallBotIndex(BallBot[] x) {
-        for(int i = 0; i < x.length; i = i = 1) {
+        for(int i = 0; i < x.length; i = i + 1) {
             if(x[i] == null) {
                 return i;
             }
         }
         return x.length;
+    }
+    
+    public double distanceBetweenPoints(TGPoint point1, TGPoint point2) {
+        return Math.sqrt((point1.x - point2.x)*(point1.x - point2.x) + (point1.y - point2.y)*(point1.y - point2.y));
+    }
+    
+    public boolean entranceClear(BallBot[] array, TGPoint coord) {
+        boolean ableToForm = true;
+        for(int index = 0; index < array.length; index++) {
+                if (array[index] != null) {
+                    TGPoint entrancePoint = new TGPoint(0,0);
+                    if(distanceBetweenPoints(array[index].getPoint(), coord) < array[index].getRadius() * 2){
+                        ableToForm = false;
+                    }
+                }
+            }
+        return ableToForm;
     }
 }
