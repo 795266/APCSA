5ce820e35e8eadaacf5cdf9d02d548733eb69f6c
795266 <42599270+795266@users.noreply.github.com>
2018-10-28 23:25:29 -0700

    Started Benchmark1 - unfinished

---
+++ ArrayList Project 2/StudentList.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class StudList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudentList
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class StudList
+     */
+    public StudentList()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getGreeting()
+    {
+        // put your code here
+        return "You have entered the teach portal";
+    }
+
+    public String getResponse(String x)
+    {
+        // put your code here
+        return "x + asdfghjkl";
+    }
+}
