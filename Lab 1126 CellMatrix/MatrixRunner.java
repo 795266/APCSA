ea4f056757c0a64443656c5b19142fe4bd1b1652
795266 <42599270+795266@users.noreply.github.com>
2018-11-26 10:34:43 -0800

    Making Lab 1126 nice

--- Lab 1126 CellMatrix/MatrixRunner.java
+++ Lab 1126 CellMatrix/MatrixRunner.java
@@ -10,10 +10,13 @@ public class MatrixRunner{
     public static void main(){
       CellMatrix cm = new CellMatrix(3, 4);
       cm.loadMatrix();
+      System.out.println("The Matrix:");
       cm.printMatrix();
       System.out.println("");
+      System.out.println("Value of the neighbors of each matrix coordinate:");
       cm.loadNeighbors();
       System.out.println("");
+      System.out.println("The value of the neighbors of the coordinate:");
       System.out.println(cm.getGreatestNeighbors());
     }
 }
